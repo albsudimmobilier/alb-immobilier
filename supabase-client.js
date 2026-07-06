6661945db3914782eb01fd0429180ef08a5db929
albsudimmobilier <albsudimmobilier@gmail.com>
2026-07-06 16:46:23 +0200

    🔧 Ajouter supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://kutbxyinpokebjdemlnq.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_wXbJu1TP2jZ05TcuMOut9Q_NcumnpIQ';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
