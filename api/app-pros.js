dfa29e8055a13ca56cf6a5a15be9234a87816cc1
albsudimmobilier <albsudimmobilier@gmail.com>
2026-07-31 20:15:22 +0200

    Update app-pros.js

--- api/app-pros.js
+++ api/app-pros.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.0/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 // Supabase Client
 const SUPABASE_URL = 'https://kutbxyinpogebjdemlnq.supabase.co';
